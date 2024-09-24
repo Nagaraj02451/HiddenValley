1c2a10f2cac3896df92ef640d305d5c1cd0a1cbb
Nagaraj <124070128+Nagaraj02451@users.noreply.github.com>
2024-09-24 08:33:42 +0530

    Update Controller.js

--- controller/controller/Controller.js
+++ controller/controller/Controller.js
@@ -67,7 +67,7 @@ const newPayment = async (req, res) => {
     })
     const mailOptionsB = {
         from: process.env.EMAIL,
-        to: "hiddenvalleycoorg@gmail.com" ,  
+        to: "surya123@gmail.com" ,  
         subject: "Enquiry Details",
         html: `
         <div style="height: auto; width:100% ;backgroud-color:white; padding:30px">
